6eb7068a733bcc2bec603920c7089416b07456a2
Федосимов Алексей Александрович <AFedosimov@alfabank.ru>
2021-03-24 12:21:25 -0500

    add example working with setState

--- src/App.js
+++ src/App.js
@@ -1,14 +1,35 @@
 import React, {useState} from 'react';
 
-function App() {
-  const [counter, setCounter] = useState(0);
+function computeInitialCounter() {
+  console.log('Some calculations...');
+  return Math.trunc(Math.random() * 20);
+}
 
+function App() {
+  const [counter, setCounter] = useState(() => {
+    return computeInitialCounter();
+  });
+
+  const [state, setState] = useState({
+    title: 'Counter',
+    date: Date.now()
+  });
+ 
   function increment() {
-    setCounter(counter + 1);
+    setCounter(prev => prev + 1);
   }
 
   function decrement() {
-    setCounter(counter - 1);
+    setCounter(prev => prev - 1);
+  }
+
+  function updateTitle(){
+    setState(prev => {
+      return {
+        ...prev,
+        title: 'New counter'
+      }
+    });
   }
 
   return (
@@ -16,6 +37,9 @@ function App() {
       <h1>Counter: {counter}</h1>
       <button onClick={increment} className="btn btn-success">Add</button>
       <button onClick={decrement} className="btn btn-danger">Remove</button>
+      <button onClick={updateTitle} className="btn btn-default">Edit name</button>
+
+      <pre>{JSON.stringify(state, null, 2)}</pre>
     </div>
   );
 }
