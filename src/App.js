7528820cbb0817717ba2ef28a6a2217b127a815b
Федосимов Алексей Александрович <AFedosimov@alfabank.ru>
2021-03-31 13:00:04 -0500

    add useMemo

--- src/App.js
+++ src/App.js
@@ -1,33 +1,32 @@
-import React, {useState, useEffect, useRef} from 'react';
+import React, {useState, useMemo, useEffect} from 'react';
 
-// let renderCount = 1; // 2
+function complexCompute(num) {
+  let i = 0;
+  while (i < 1000000000) i++;
+  return num * 2;
+}
 
 function App() {
-  // const [renderCount, setRenderCount] = useState(1); // 1
-  const [value, setValue] = useState('initial');
-  const renderCount = useRef(1);
-  const inputRef = useRef(null);
-  const prevValue = useRef('');
+  const [number, setNumber] = useState(42);
+  const [colored, setColored] = useState(false);
+  const styles = useMemo(() => ({
+    color: colored ? 'darkred' : 'blue'
+  }), [colored]);
+
+  const computed = useMemo(() => {
+    return complexCompute(number);
+  }, [number]);
 
   useEffect(() => {
-    // setRenderCount(prev => prev + 1); // 1
-    // renderCount++; // 2
-    renderCount.current++;
-    console.log(inputRef.current.value);
-  });
-
-  useEffect(() => {
-    prevValue.current = value;
-  }, [value]);
-
-  const focus = () => inputRef.current.focus();
+    console.log('Styles changed');
+  }, [styles]);
 
   return (
     <div>
-      <h1>Count of renders: {renderCount.current}</h1>
-      <h2>Previous value: {prevValue.current}</h2>
-      <input ref={inputRef} type="text" onChange={e => setValue(e.target.value)} value={value} />
-      <button className="btn btn-success" onClick={focus}>Focus</button>
+      <h1 style={styles}>Computed property: {computed}</h1>
+      <button className={'btn btn-success'} onClick={() => setNumber(prev => prev + 1)}>Add</button>
+      <button className={'btn btn-danger'} onClick={() => setNumber(prev => prev - 1)}>Remove</button>
+      <button className={'btn btn-warning'} onClick={() => setColored(prev => !prev)}>Edit</button>
     </div>
   );
 }
