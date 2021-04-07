17aeaa0b055f2a40638f40e7ab4377057a95baf6
Федосимов Алексей Александрович <AFedosimov@alfabank.ru>
2021-04-07 13:47:49 -0500

    add more hooks

--- src/App.js
+++ src/App.js
@@ -1,16 +1,39 @@
-import React from 'react';
-import Alert from './alert/Alert';
-import { AlertProvider } from './alert/AlertContext';
-import Main from './Main';
+import React, {useState, useEffect} from 'react';
+
+function useLogger(value) {
+  useEffect(() => {
+    console.log('Value changed: ', value);
+  }, [value]);
+}
+
+function useInput(initialValue) {
+  const [value, setValue] = useState(initialValue);
+
+  const onChange = event => {
+    setValue(event.target.value);
+  }
+
+  const clear = () => setValue('');
+
+  return {
+    bind: {value, onChange},
+    value,
+    clear
+  };
+}
 
 function App() {
+  const input = useInput('');
+
+  useLogger(input.value);
+
   return (
-    <AlertProvider>
-      <div className={'container pt-3'}>
-        <Alert />
-        <Main />
-      </div>
-    </AlertProvider>
+    <div className={'container pt-3'}>
+      <input type="text" {...input.bind} />
+      <button className="btn btn-warning" onClick={() => input.clear()}>Clear</button>
+      <hr/>
+      <h1>{input.value}</h1>
+    </div>
   );
 }
 
